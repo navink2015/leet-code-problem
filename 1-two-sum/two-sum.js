a8a098a10e56567185aae05e00d1f414f354ced5
navin <navinkumar.manoharan@kaaylabs.com>
2025-10-30 22:28:05 +0530

    1 two sum

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,21 @@
+const { jsx } = require("react/jsx-runtime");
+
+const nums = [2,7,11,15], target = 18;
+
+var twoSum=(nums, target)=>{
+    const hasSet = new Map();
+    for(var i=0; i<nums.length; i++) {
+        const reminderValue =  target - nums[i] ;
+        console.log(hasSet, reminderValue)
+        if(hasSet.has(reminderValue)){
+            return [i, hasSet.get(reminderValue)]
+        }
+        hasSet.set(nums[i], i)
+    }
+}
+
+const result = twoSum(nums, target);
+console.log(result);
+
+// time complexity 0(n)
+// space complexity o(n)
